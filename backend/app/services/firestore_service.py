1548d12f3886475c28e74f63f8d948b900435d98
Sahil-Ghadi <sahilghadi514@gmail.com>
2025-10-23 19:14:31 +0530

    service and store

---
+++ backend/app/services/firestore_service.py
@@ -0,0 +1,16 @@
+from app.config import db
+
+def create_document(collection_name: str, doc_id: str, data: dict):
+    db.collection(collection_name).document(doc_id).set(data)
+    return data
+
+def get_document(collection_name: str, doc_id: str):
+    doc_ref = db.collection(collection_name).document(doc_id)
+    doc = doc_ref.get()
+    if doc.exists:
+        return doc.to_dict()
+    return None
+
+def get_collection(collection_name: str):
+    docs = db.collection(collection_name).stream()
+    return [doc.to_dict() for doc in docs]
